245f69423f7f842cd2f18b5f831f7febf457d6aa
Wcb-2000 <846583697@qq.com>
2021-05-31 23:28:12 +0800

    导航栏初始化

--- vue-practice/src/main.js
+++ vue-practice/src/main.js
@@ -2,7 +2,7 @@
  * Author: 吴楚标
  * Date: 2021-05-30 17:46:59
  * LastEditors: 吴楚标
- * LastEditTime: 2021-05-31 20:22:59
+ * LastEditTime: 2021-05-31 23:22:35
  * Description:
 */
 // The Vue build version to load with the `import` command
@@ -11,8 +11,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/style/reset.css'
-import './assets/style/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)

c413b8c737e6ff6a53debd87b46923badb21cefc
Wcb-2000 <846583697@qq.com>
2021-06-01 11:08:16 +0800

    轮播图upgrade

--- vue-practice/src/main.js
+++ vue-practice/src/main.js
@@ -2,7 +2,7 @@
  * Author: 吴楚标
  * Date: 2021-05-30 17:46:59
  * LastEditors: 吴楚标
- * LastEditTime: 2021-05-31 23:22:35
+ * LastEditTime: 2021-06-01 10:34:14
  * Description:
 */
 // The Vue build version to load with the `import` command
@@ -11,12 +11,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/swiper-bundle.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+/*  Vue.use(VueAwesomeSwiper),/* { default options with global component } */
 
 /* eslint-disable no-new */
 new Vue({

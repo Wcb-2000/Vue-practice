709fc5beab24247bcfc89ca7ba638636bfb09426
Wcb-2000 <846583697@qq.com>
2021-06-02 19:51:44 +0800

    城市选择css初始化,upgrade

--- vue-practice/src/pages/city/City.vue
+++ vue-practice/src/pages/city/City.vue
@@ -2,24 +2,30 @@
  * @Author: 吴楚标
  * @Date: 2021-06-02 10:55:38
  * @LastEditors: 吴楚标
- * @LastEditTime: 2021-06-02 11:44:35
+ * @LastEditTime: 2021-06-02 19:46:40
  * @Description:
 -->
 <template>
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
---
+++ vue-practice/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: 吴楚标
+ * @Date: 2021-06-02 19:44:11
+ * @LastEditors: 吴楚标
+ * @LastEditTime: 2021-06-02 19:50:56
+ * @Description:
+-->
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+  display: flex
+  flex-direction: column
+  justify-content: center
+  position: absolute
+  top:1.58rem
+  right: 0
+  bottom: 0
+  width: .4rem
+  .item
+    line-height: .4rem
+    text-align: center
+    color: $bgColor
+</style>

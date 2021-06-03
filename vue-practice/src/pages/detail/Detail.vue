a9d9dd821524867f44e68d02991eede739ee5475
Wcb-2000 <846583697@qq.com>
2021-06-03 23:54:10 +0800

    详情页-头部渐显

--- vue-practice/src/pages/detail/Detail.vue
+++ vue-practice/src/pages/detail/Detail.vue
@@ -2,23 +2,30 @@
  * @Author: 吴楚标
  * @Date: 2021-06-03 14:47:59
  * @LastEditors: 吴楚标
- * @LastEditTime: 2021-06-03 15:34:43
+ * @LastEditTime: 2021-06-03 23:04:51
  * @Description:
 -->
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+  .content
+    height: 50rem
+</style>

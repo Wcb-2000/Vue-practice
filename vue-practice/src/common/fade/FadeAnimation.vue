ef7a000defd08b7d1ee47fc31ea8a3e884e5d5fd
Wcb-2000 <846583697@qq.com>
2021-06-04 16:19:08 +0800

    图片点击动画效果

---
+++ vue-practice/src/common/fade/FadeAnimation.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: 吴楚标
+ * @Date: 2021-06-04 15:59:52
+ * @LastEditors: 吴楚标
+ * @LastEditTime: 2021-06-04 16:18:37
+ * @Description:
+-->
+<template>
+  <transition>
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Fade'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .v-enter, .v-leave-to
+    opacity: 0
+  .v-enter-active, .v-leave-active
+    transition: opacity .3s
+</style>
